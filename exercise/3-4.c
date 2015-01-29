4290fe27285bd562e8ab5cb9cf294a3409468af0
BibleSon <bibleson507@naver.com>
2015-01-29 13:20:00 +0900

    exercise 3-4 complete

---
+++ exercise/3-4.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+	int his, lit, art, tot;
+	double avg;
+
+	printf("역사, 문학, 예능 점수를 입력하세요 : ");
+	scanf("%d %d %d",&his,&lit,&art);
+
+	tot = his + lit + art;
+	avg = tot/3.0;
+
+	printf("총점은 %d이고 평균은 %lf입니다.",tot,avg);
+
+	return 0;
+}

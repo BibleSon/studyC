e651266a4ceae6afb0802c6c739036cc29986bf2
Jake Yoon <yjaeseok@gmail.com>
2015-01-29 12:54:29 +0900

    Exercise 3-1 pay -> charge

--- exercise/3-1.c
+++ exercise/3-1.c
@@ -2,13 +2,13 @@
 int main(void)
 {
 	int kw;
-	double basepay = 660;
-	double kwperpay = 88.5;
+	double basecharge = 660;
+	double kwpercharge = 88.5;
 	double tax = 0.09;
-	double pay;
+	double charge;
 	printf("전기 사용량을 입력하세요(kw) : ");
 	scanf("%d", &kw);
-	pay = (basepay + (kw * kwperpay)) * (1 + tax);
-	printf("전기 사용 요금은 %lf원입니다.\n", pay);
+	charge = (basecharge + (kw * kwpercharge)) * (1 + tax);
+	printf("전기 사용 요금은 %lf원입니다.\n", charge);
 	return 0;
 }

c5f09cde147631638bfc721ad86998e65de66576
BibleSon <bibleson507@naver.com>
2015-01-29 11:31:22 +0900

    exercise 2-3 complete

---
+++ exercise/2-3.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+	int height = 168;
+	int weight = 48;
+	int cha;
+	cha = height - weight ;
+	
+	printf("키 : %dcm \n몸무게 : %dkg \n키에서 몸무게를 뺀 값은 %d 입니다.",height,weight,cha);
+
+	return 0;
+}

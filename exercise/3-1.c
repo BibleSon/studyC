5f08bbfca5b7ebe8042d703d4eb0646edf3b2f77
BibleSon <bibleson507@naver.com>
2015-01-29 12:56:25 +0900

    exercise 3-1 complete

---
+++ exercise/3-1.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main()
+{
+	int kw;
+	double cost, totalcost;
+
+
+
+	printf("전기 사용량을 입력하세요(kw) :");
+	scanf("%d",&kw);
+
+	cost = kw*88.5+660;
+	totalcost = cost + (cost*0.09);
+
+	printf("전기 사용 요금은 %lf원 입니다.",totalcost);
+	return 0;
+
+}

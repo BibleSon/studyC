c8733b6d22e5b4fcc974f7da7f293943f9fdd2a1
Jake Yoon <yjaeseok@gmail.com>
2015-01-29 12:58:42 +0900

    Exercise 3-2 끝

---
+++ exercise/3-2.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main(void)
+{
+	char num;
+	double bat;
+	int age;
+	
+	printf("등번호를 입력하세요 : ");
+	scanf("%c", &num);
+	printf("타율을 입력하세요 : ");
+	scanf("%lf", &bat);
+	printf("나이를 입력하세요 : ");
+	scanf("%d", &age);
+	printf("%c번 선수의 타율은 %lf이고 나이는 %d살입니다.\n", num, bat, age);
+	return 0;
+}
+

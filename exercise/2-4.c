fbb9831868386474b4a05e5f0949066119668f0e
BibleSon <bibleson507@naver.com>
2015-01-29 11:43:09 +0900

    exercise 2-4 complete

---
+++ exercise/2-4.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+	int num1, num2, num3;
+	
+	printf("두 정수값을 입력하세요 :");
+	scanf("%d%d",&num1,&num2);
+	num3 = num1 + num2;
+	printf("두 정수의 합은 %d입니다.",num3);
+
+	return 0;
+}
+
+

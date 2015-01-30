385dda1cdcc1e1666f0a71e9242d3938f59474df
BibleSon <bibleson507@naver.com>
2015-01-30 14:50:13 +0900

    5-2 complete

---
+++ bibleson/exercise/5-2.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+    int d1, d2;
+    printf("두 정수를 입력하세요 :");
+    scanf("%d%d",&d1,&d2);
+    
+    
+
+    printf("나눗셈 연산식은 \"%d / %d = %lf\"입니다.",d1,d2,(double)d1/d2);
+
+    return 0;
+}

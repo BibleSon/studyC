156638fa0fe03b70b85a6f9f9a8d12c651ff64e4
Jake Yoon <yjaeseok@gmail.com>
2015-01-30 11:00:24 +0900

    Exercise 4-3 끝

---
+++ jake/exercise/4-3.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void)
+{
+    unsigned int age;
+    unsigned int t;
+    printf("몇 살까지 살고 싶습니까? ");
+    scanf("%u", &age);
+    
+    printf("%u일, %u시간, %u분, %u초입니다.\n", age * 365, age * 365 * 24, age * 365 * 24 * 60, age * 365 * 24 * 60 * 60);
+    return 0;
+}
+

b7e37ec482a2531c228a255ea3f576758af9d64b
Jake Yoon <yjaeseok@gmail.com>
2015-01-30 11:39:44 +0900

    Exercise 4-5 끝

--- jake/exercise/4-4.c
+++ jake/exercise/4-4.c
@@ -3,8 +3,14 @@
 int main(void)
 {
     int dist, speed;
-    printf("거리(km)와 속력(km/h)을 입력하세요 : ");
+    double t;
+    printf("거리와 속력을 입력하세요 : ");
     scanf("%d%d", &dist, &speed);
-    printf("%d시간 걸리고 %d(km)가 남습니다.\n", dist / speed, dist % speed);
+    t = ((dist * 1.0f) / speed);
+   // printf("%d시간 걸리고 %d(km)가 남습니다.\n", dist / speed, dist % speed);
+    printf("걸리는 시간은 : %d시간, %d분, %.3f초입니다. \n", 
+            (int)t, 
+            (int)(t * 60 - (int)t * 60), 
+            (t * 60 * 60) - (int)(t * 60) * 60);
     return 0;
 }

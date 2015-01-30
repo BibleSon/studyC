b882f63631927c872ac77b2320e18db4f096766b
Jake Yoon <yjaeseok@gmail.com>
2015-01-30 10:56:45 +0900

    Exercise 4-2 끝

--- jake/exercise/4-2.c
+++ jake/exercise/4-2.c
@@ -6,7 +6,7 @@ int main(void)
     
     printf("정수값을 입력하세요 : ");
     scanf("%d", &n);
-    printf("입력된 값은 8진수로 %06o입니다.\n", n);
-    printf("입력된 값은 16진수로 0x%x입니다.\n", n);
+    printf("입력된 값은 8진수로 %#o입니다.\n", n);
+    printf("입력된 값은 16진수로 %#x입니다.\n", n);
     return 0;
 }

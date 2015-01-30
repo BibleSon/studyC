704c70449eb1f5029eff95d28c324e5a635e02f9
BibleSon <bibleson507@naver.com>
2015-01-30 11:08:45 +0900

    4-2 4-3 complete

---
+++ bibleson/exercise/4-2.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+    int n;
+
+	printf("정수값을 입력하세요 :");
+	scanf("%d",&n);
+    printf("입력된 값은 8진수로 %#o입니다.\n입력된 값은 16진수로 %#x입니다.",n,n);
+
+    return 0;
+}
+
---
+++ bibleson/exercise/4-3.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main()
+{
+    unsigned int age, day, hour, min, sec;
+    
+    printf("몇 살까지 살고 싶습니까?");
+    scanf("%d",&age);
+
+    if(age<70){printf("오래사셔야죠^^ 다시 입력해주세요.");}
+    else {
+        day = 365*age;
+        hour = day*24;
+        min = hour*60;
+        sec = min*60;
+        printf("%u일, %u시간, %u분, %u초입니다.",day,hour,min,sec);
+    }
+
+    return 0;
+}

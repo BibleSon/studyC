8ade2aea3710c8637809d6a0418e2550368c4581
BibleSon <bibleson507@naver.com>
2015-02-02 10:31:39 +0900

    lecture complete

--- bibleson/algospot/lecture.c
+++ bibleson/algospot/lecture.c
@@ -3,7 +3,7 @@
 
 int main()
 {
-    int tmp1, tmp2, len, i, n;
+    int tmp1, tmp2, len, m, i, n;
     char string[10000];
 
     scanf("%d",&n);
@@ -11,23 +11,30 @@ int main()
     while(n--){
         scanf("%s",string);
         len = strlen(string);
-        while(len--){
+        while(len>2){
             for(i=0;i<len-2;i+=2){
-                if(string[i]>string[i+2]){
-                    if(string[i+1]>string[i+3]){
+                if(((int)string[i]>(int)string[i+2])){
                             tmp1 = string[i];
                             tmp2 = string[i+1];
                             string[i] = string[i+2];
                             string[i+1] = string[i+3];
                             string[i+2] = tmp1;
                             string[i+3] = tmp2;
-                    }
+                }
+                else if(((int)string[i]==(int)string[i+2])&&((int)string[i+1]>(int)string[i+3])){
+                    tmp1 = string[i];
+                    tmp2 = string[i+1];
+                    string[i] = string[i+2];
+                    string[i+1] = string[i+3];
+                    string[i+2] = tmp1;
+                    string[i+3] = tmp2;
                 }
             }
-        }
+            len = len -2;
+            }
         printf("%s\n",string);
-    } 
-    return 0;
+    }
+        return 0;
 }
 
                             

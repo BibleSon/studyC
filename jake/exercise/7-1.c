7a8ff46458ca31cc59f2147550e19862c3e91e3a
Jake Yoon <yjaeseok@gmail.com>
2015-02-04 11:23:53 +0900

    7-1 끝

---
+++ jake/exercise/7-1.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int n = 1;
+    int sum = 0;
+    while(1) {
+        printf("양수를 입력하세요 : ");
+        scanf("%d", &n);
+        
+        if (n < 0) {
+            break;
+        }
+        sum += n;
+        printf("지금까지 입력된 값의 합 : %d\n", sum);
+    }
+    return 0;
+}
+

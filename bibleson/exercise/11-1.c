9e78a4c84cc13ecba56aa9519700299cb80696dd
BibleSon <bibleson507@naver.com>
2015-02-06 16:55:26 +0900

    11-1 complete

---
+++ bibleson/exercise/11-1.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main()
+{
+    double ary[] = {1.5, 20.1, 16.4, 2.3, 3.5};
+    int m, i;
+    double tot, avg;
+
+    tot = 0;
+
+    m = sizeof(ary)/sizeof(ary[0]);
+
+    for(i=0;i<m;i++){
+        tot = tot + *(&ary[0] + i);
+    }
+    
+    avg = tot / m;
+    
+    printf("평균값 : %.2lf",avg);
+    return 0;
+    
+}
+

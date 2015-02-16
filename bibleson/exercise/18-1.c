b55bf3a1edc860722d9bfe212d046fac42ce3c0b
BibleSon <bibleson507@naver.com>
2015-02-16 17:19:59 +0900

    18-1 complete

--- bibleson/exercise/18-1.c
+++ bibleson/exercise/18-1.c
@@ -2,8 +2,28 @@
 
 int main()
 {
+    int i = 0;
+    char ch, temp1='0', temp2='0';
+    FILE *fp;
+    fp = fopen("a.txt","r");
+
+    if(fp==NULL){
+        printf("파일이 없습니다.");
+        return 1;
+    }
     
-    
+    while(1){
+        ch = fgetc(fp);
+        if(ch == EOF) break;
+        putchar(ch);
+        temp2 = temp1;
+        temp1 = ch;
+        if((temp2 == '.')&&(temp1 == ' ')){ putchar('\n'); i++; }
+        if(i == 3){ putchar('\n'); i=0; }
+    }
+
+    fclose(fp);
+
     return 0;
 }
 
